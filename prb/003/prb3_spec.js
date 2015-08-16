383c53176d5eab317d19adfbd881e5fa2ceae8bc
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-08-16 21:01:43 +0200

    Add problem 3 function and its Jasmine test
    - Write solution of the problem 3
    - Add custom matcher toBeEven to Jasmine test

---
+++ prb/003/prb3_spec.js
@@ -0,0 +1,24 @@
+describe('Problem #3 function', function(){
+  beforeAll(function() {
+    jasmine.addMatchers({
+      toBeEven: function(util, customEqualityTesters) {
+	return {
+	  compare: function(actual){
+	    return { pass: util.equals(actual%2==0, true, customEqualityTesters) }
+	  }
+	}
+	this.message = function() {
+	  return "Expected " + actual + " to be even";
+	}
+      }
+    });
+  });
+
+  it('finds the correct result', function(){
+    expect(prb3()).toEqual(6857);
+  });
+
+  it('expect the result not to be even', function(){
+    expect(prb3()).not.toBeEven();
+  });
+});

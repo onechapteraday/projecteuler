aaac4edb2ed5496ff96ada162d2bc1eb06dc5b5c
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-09-30 23:04:49 +0200

    Add problem 5 function and its Jasmine test

---
+++ prb/005/prb5.js
@@ -0,0 +1,8 @@
+/* 2520 is the smallest number that can be divided by each of the numbers from
+ * 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all the
+ * numbers from 1 to 20?*/
+
+function prb5(){
+  return 2*3*5*7*11*13*17*19*2*2*3*2;
+}
---
+++ prb/005/prb5_spec.js
@@ -0,0 +1,5 @@
+describe('Problem #5 function', function(){
+  it('finds the correct result', function(){
+    expect(prb5()).toEqual(232792560);
+  });
+});

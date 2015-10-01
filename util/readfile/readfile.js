bad0941401bd966e618efe30a4c239d68924fa80
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-10-01 23:03:49 +0200

    Add readfile function to util directory

---
+++ util/readfile/readfile.js
@@ -0,0 +1,15 @@
+function readFile(file, make){
+  var xhttp, data = '';
+
+  if (window.XMLHttpRequest) xhttp = new XMLHttpRequest();
+  else xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  
+  xhttp.open('GET', file, true);
+  xhttp.overrideMimeType('text/plain');
+  xhttp.send();
+
+  xhttp.onreadystatechange = function (){
+    if(xhttp.readyState == 4)
+      make(xhttp.responseText);
+  }
+}

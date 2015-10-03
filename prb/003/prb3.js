5aa7d63179fc71d941706e5ad80bba1a10d7d9cb
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-10-03 22:10:50 +0200

    Update problem 3 function and its html file
    - Factorization of code, use of the prime factorization function
    - This works only because the factors function returns a sorted array

--- prb/003/prb3.js
+++ prb/003/prb3.js
@@ -2,22 +2,6 @@
  * What is the largest prime factor of the number 600851475143? */
 
 function prb3(){
-  var x = 600851475143,
-      largest = 1,
-      k = 3;
-  if(x % 2 == 0){
-    x = x / 2;
-    largest = 2;
-    while(x % 2 == 0) x = x / 2;
-  }
-  while(x > 1 && k <= Math.sqrt(x)){
-    if(x % k == 0){
-      largest = k;
-      x = x / k;
-      while(x % k == 0) x = x / k;
-    }
-    k += 2;
-  }
-  if(x == 1) return largest;
-  else return x;
+  var f = factors(600851475143);
+  return f[f.length-1];
 }

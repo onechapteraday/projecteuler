c8af7e398533b1ec64afec1ac6f1b21e5b62433a
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-10-04 12:19:33 +0200

    Update factorial function and its html file
    - Use of memoizer to optimize the function

--- util/factorial/factorial.js
+++ util/factorial/factorial.js
@@ -1,6 +1,7 @@
 /* In mathematics, the factorial of a non-negative integer n, denoted by n!, is 
- * the product of all positive integers less than or equal to n. */
+ * the product of all positive integers less than or equal to n.
+ * This function now use memoization. */
 
-function factorial(n){
-  return (n<0)?undefined:(n==0 || n==1)?1:n*factorial(n-1);
-}
+var factorial = memoizer([1,1], function(shell,n){
+  return (n<0)?undefined:n*shell(n-1);
+});

d1593e759a306ccdf3c18e452b47aa9244f56d21
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-10-05 13:42:35 +0200

    Add maxpath function
    - For now, the grid is included in the function

---
+++ util/maxpath/maxpath.js
@@ -0,0 +1,24 @@
+var maxpath = function(){
+  var cache = new Array();
+  for(var i = 0 ; i < 15 ; i++){
+    cache[i] = new Array();
+  }
+  var max = function(g,x,y){
+    var result = cache[x][y];
+    if(typeof(result) == "undefined"){
+      if(x==0 && y==0) return g[x][y];
+      else{
+        if(typeof(g[x-1][y])=="undefined"){
+          return cache[x][y] = g[x][y] + max(g,x-1,y-1); /* right side */
+        }else if(typeof(g[x-1][y-1])=="undefined"){
+          return cache[x][y] = g[x][y] + max(g,x-1,y); /* left side */
+        }else{ /* middle */
+          return cache[x][y] = g[x][y] + Math.max(max(g,x-1,y-1),max(g,x-1,y));
+        }
+      }
+    }else{
+      return result;
+    }    
+  };
+  return max;
+}();

2aef92d32f9ece58885ed17f2e1d1f0ea298f96c
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-10-05 08:24:05 +0200

    Update problem 11 function
    - Fix characters encoding problems, I think

--- prb/011/prb11.js
+++ prb/011/prb11.js
@@ -1,8 +1,8 @@
-/* In the 20×20 grid below, four numbers along a diagonal line have been marked in
+/* In the 20x20 grid below, four numbers along a diagonal line have been marked in
  * red. (cf. prb11.txt)
- * The product of these numbers is 26 × 63 × 78 × 14 = 1788696. 
+ * The product of these numbers is 26 x 63 x 78 x 14 = 1788696. 
  * What is the greatest product of four adjacent numbers in the same direction 
- * (up, down, left, right, or diagonally) in the 20×20 grid? */
+ * (up, down, left, right, or diagonally) in the 20x20 grid? */
 
 function prb11(input){
   var grid = new Array(),

321b7058491e87d65f70f76e336cbb5d5b2797f7
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-10-06 23:49:17 +0200

    Update problem 3 Jasmine test
    - Fix unreachable code after return statement

--- prb/003/prb3_spec.js
+++ prb/003/prb3_spec.js
@@ -2,13 +2,13 @@ describe('Problem #3 function', function(){
   beforeAll(function() {
     jasmine.addMatchers({
       toBeEven: function(util, customEqualityTesters) {
+	this.message = function() {
+	  return "Expected " + actual + " to be even";
+	}
 	return {
 	  compare: function(actual){
 	    return { pass: util.equals(actual%2==0, true, customEqualityTesters) }
 	  }
-	}
-	this.message = function() {
-	  return "Expected " + actual + " to be even";
 	}
       }
     });

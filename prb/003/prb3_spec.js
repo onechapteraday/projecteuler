ad157ea10954d6878951f565f49e412e3db52fd6
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-10-14 20:05:24 +0200

    Update problems 3 and 4 to have only one spec

--- prb/003/prb3_spec.js
+++ prb/003/prb3_spec.js
@@ -1,24 +1,5 @@
 describe('Problem #3 function', function(){
-  beforeAll(function() {
-    jasmine.addMatchers({
-      toBeEven: function(util, customEqualityTesters) {
-	this.message = function() {
-	  return "Expected " + actual + " to be even";
-	}
-	return {
-	  compare: function(actual){
-	    return { pass: util.equals(actual%2==0, true, customEqualityTesters) }
-	  }
-	}
-      }
-    });
-  });
-
   it('finds the correct result', function(){
     expect(prb3()).toEqual(6857);
-  });
-
-  it('expect the result not to be even', function(){
-    expect(prb3()).not.toBeEven();
   });
 });
--- prb/004/prb4_spec.js
+++ prb/004/prb4_spec.js
@@ -1,8 +1,4 @@
 describe('Problem #4 function', function(){
-  it('finds a palindrome', function(){
-    expect(ispalindrome(prb4())).toBe(true);
-  });
-
   it('finds the correct result', function(){
     expect(prb4()).toEqual(906609);
   });

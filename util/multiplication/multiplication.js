4b68502ac8714908c044ae04651cc93ac5e6cd95
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-10-15 21:36:18 +0200

    Update multiplication function to be more efficient

--- util/multiplication/multiplication.js
+++ util/multiplication/multiplication.js
@@ -1,67 +1,41 @@
 /* These functions are created because native JavaScript don't allow big Integer 
  * operations. These is an implementation of the multiplication. */
 
-function multiply(x,y){
-     return parseInt(x) * parseInt(y);
-}
 function multiplication(x,y){
-  var xdigits = (""+x).split(""),
-      ydigits = (""+y).split(""),
-      greater = new Array(),
-      smaller = new Array(),
+  if(x.toString()=="0" || y.toString()=="0") return '0';
+
+  var greater = ((x.length > y.length)||(x.length == y.length && x > y))?x:y,
+      smaller = ((x.length > y.length)||(x.length == y.length && x > y))?y:x,
       lines = new Array(),
-      xlength = xdigits.length,
-      ylength = ydigits.length,
       length = 0,
-      sum = "",
+      sum,
       result = 0,
-      retenue = 0;
-  // Who's the greater number ?
-  if( (xlength>ylength) || (xlength==ylength && x > y)){
-    greater = xdigits;
-    greater_length = xlength;
-    smaller = ydigits;
-    smaller_length = ylength;
-  }else{
-    greater = ydigits;
-    greater_length = ylength;
-    smaller = xdigits;
-    smaller_length = xlength;
-  }
+      retenue = 0,
+      final_result = [0];
+  
   // Make the multiplication
-  for(var i = smaller_length; i > 0; i--){
-    sum = "";
+  for(var i = smaller.length; i > 0; i--){
+    sum = [];
     retenue = 0;
-    if(i<smaller_length){
-      var f = smaller_length - i;
-      for(k = 0; k < f; k++){
-        sum = "0".toString().concat(sum);
-      }
+    if(i<smaller.length){
+      var f = smaller.length - i;
+      for(k = 0; k < f; k++)
+        sum.unshift(0);
     }
-    for(var j = greater_length; j > 0; j-- ){
-      result = addition(multiply(smaller[i-1],greater[j-1]),retenue);
-      if(result.toString().length>1){
-        digits = (""+result.toString()).split("");
-        sum = digits[1].toString().concat(sum);
-        retenue = digits[0];
-      }else{
-        sum = result.toString().concat(sum);
-        retenue = 0;
-      }
+    for(var j = greater.length; j > 0; j-- ){
+      result = smaller[i-1]*greater[j-1]+retenue;
+      sum.unshift(result%10000000);
+      retenue = ~~(result/10000000);
     }
-    if(retenue>0) sum = retenue.toString().concat(sum);
+    if(retenue>0) sum.unshift(retenue);
     lines[lines.length] = sum;
   }
-  var lines_length = lines.length,
-      final_result = 0;
-  if(lines_length>1){
-    for(var i = 0; i < lines_length; i++){
-      final_result = addition(final_result,lines[i]);
-    }
+
+  if(lines.length>1){
+    for(var i = 0; i < lines.length; i++)
+      final_result = this.addition(final_result,lines[i]);
     sum = final_result;
   }
-  // Display sum
-  var regex = /^0+$/g;
-  if(sum.match(regex)) return '0';
-  else return sum;
+  
+  return sum;
 }
--- util/multiplication/multiplication_spec.js
+++ util/multiplication/multiplication_spec.js
@@ -1,27 +1,27 @@
 describe('Multiplication function', function(){
-  var a = multiplication('147586321412563694559633','5874532698962144366844'),
-      b = multiplication('999999999999999999999','1'),
-      c = multiplication('0','4378'),
-      d = multiplication('9999','9999'),
-      e = multiplication('1233434323432454521234','99872343237868642879');
+  var a = multiplication(new LargeNumber('147586321412563694559633').getArray(), new LargeNumber('5874532698962144366844').getArray()),
+      b = multiplication(new LargeNumber('999999999999999999999').getArray(), new LargeNumber('1').getArray()),
+      c = multiplication(new LargeNumber('0').getArray(), new LargeNumber('4378').getArray()),
+      d = multiplication(new LargeNumber('9999').getArray(), new LargeNumber('9999').getArray()),
+      e = multiplication(new LargeNumber('1233434323432454521234').getArray(), new LargeNumber('99872343237868642879').getArray());
 
   it('finds the correct result for a', function(){
-    expect(a).toEqual('867000671057642319468574944988733500186008252');
+    expect(new LargeNumber(a).getValue()).toEqual('867000671057642319468574944988733500186008252');
   });
 
   it('finds the correct result for b', function(){
-    expect(b).toEqual('999999999999999999999');
+    expect(new LargeNumber(b).getValue()).toEqual('999999999999999999999');
   });
 
   it('finds the correct result for c', function(){
-    expect(c).toEqual('0');
+    expect(new LargeNumber(c).getValue()).toEqual('0');
   });
 
   it('finds the correct result for d', function(){
-    expect(d).toEqual('99980001');
+    expect(new LargeNumber(d).getValue()).toEqual('99980001');
   });
 
   it('finds the correct result for e', function(){
-    expect(e).toEqual('123185976111214383871838337251819268392686');
+    expect(new LargeNumber(e).getValue()).toEqual('123185976111214383871838337251819268392686');
   });
 });

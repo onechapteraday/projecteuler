f34f8f7de8d1a2037b7e56715910025f5e9ae5ab
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-10-18 16:59:57 +0200

    Update problem 1 function to be more efficient with larger numbers

--- prb/001/prb1.js
+++ prb/001/prb1.js
@@ -3,9 +3,5 @@
  * Find the sum of all the multiples of 3 or 5 below 1000. */
 
 function prb1(){
-  var sum = 0;
-  for(var i=3; i<1000; i++){
-    if(i%3==0 || i%5==0) sum += i;
-  }
-  return sum;
+  return sumofmultiples(3,1000) + sumofmultiples(5,1000) - sumofmultiples(15,1000);
 }

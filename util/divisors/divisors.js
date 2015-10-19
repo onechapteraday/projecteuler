81440918ab68b29f1dbecb0aff6b2872fc3b01a9
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-10-19 13:31:30 +0200

    Add sum of all divisors function

--- util/divisors/divisors.js
+++ util/divisors/divisors.js
@@ -1,5 +1,5 @@
 /* In mathematics, a divisor of an integer n, also called a factor of n, is an 
- * integer that can be multiplied by sime integer to produce n. */
+ * integer that can be multiplied by some integer to produce n. */
 
 function divisors(n){
   var d = [];
@@ -10,4 +10,15 @@ function divisors(n){
     }
   }
   return d.sort((x,y) => x-y);
+}
+
+function sumdivisors(n){
+  var sum = 0;
+  for(var i = 0; i <= Math.sqrt(n); i++){
+    if(n%i==0){
+      sum += i;
+      if(i*i!=n) sum += n/i;
+    }
+  }
+  return sum;
 }

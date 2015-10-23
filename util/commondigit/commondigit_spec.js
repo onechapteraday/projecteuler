31864a59b7e610e34aa901834a9ee2a5528c431b
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-10-23 14:41:06 +0200

    Update common digit function Jasmine test

--- util/commondigit/commondigit_spec.js
+++ util/commondigit/commondigit_spec.js
@@ -1,17 +1,21 @@
 describe('Common digit function', function(){
   it('expect 23 and 34 to have common digit', function(){
-    expect(parseInt(commondigit(23,34))).toEqual(3);
+    expect(parseInt(commondigit(23,34))).toBe(3);
   });
 
   it('expect 50 and 15 to have common digit', function(){
-    expect(parseInt(commondigit(50,15))).toEqual(5);
+    expect(parseInt(commondigit(50,15))).toBe(5);
   });
 
   it('expect 78 and 98 to have common digit', function(){
-    expect(parseInt(commondigit(78,98))).toEqual(8);
+    expect(parseInt(commondigit(78,98))).toBe(8);
   });
 
   it('expect 61 and 62 to have common digit', function(){
-    expect(parseInt(commondigit(62,61))).toEqual(6);
+    expect(parseInt(commondigit(62,61))).toBe(6);
+  });
+
+  it('expect 19 and 62 to be completely different', function(){
+    expect(Boolean(commondigit(19,62))).toEqual(false);
   });
 });

acf49f560a1bfde4c4f9bd06a2396d332c11614e
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-10-28 14:56:27 +0100

    Add problem 40 function and its Jasmine test

---
+++ prb/040/prb40.js
@@ -0,0 +1,11 @@
+/* An irrational decimal fraction is created by concatenating the positive integers:
+ * 0.123456789101112131415161718192021...
+ * It can be seen that the 12th digit of the fractional part is 1.
+ * If dn represents the nth digit of the fractional part, find the value of the
+ * following expression.
+ * d1 x d10 x d100 x d1000 x d10000 x d100000 x d1000000 */
+
+function prb40(){
+  var c = champernowne(1000001);
+  return parseInt(c[1]) * parseInt(c[10]) * parseInt(c[100]) * parseInt(c[1000]) * parseInt(c[10000]) * parseInt(c[100000]) * parseInt(c[1000000]);
+}
---
+++ prb/040/prb40_spec.js
@@ -0,0 +1,5 @@
+describe('Problem #40 function', function(){
+  it('finds the correct result', function(){
+    expect(prb40()).toEqual(210);
+  });
+});

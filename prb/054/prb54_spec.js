e2087172ef847a70fdd9181da77c90d23e0ff723
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-11-05 12:55:33 +0100

    Prepare problem 54 context

---
+++ prb/054/prb54_spec.js
@@ -0,0 +1,8 @@
+describe("Problem #54 function", function() {
+  it("finds the correct result", function(done) {
+    readFile('https://raw.githubusercontent.com/christellehilaricus/projecteuler/master/prb/054/poker.txt', function(data){
+      expect(prb54(data)).toEqual(true);
+      done();
+    });
+  });
+});

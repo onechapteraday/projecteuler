e470f0478a0b357dfa13b7b208ea0e55dfd04ca6
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-11-07 22:18:56 +0100

    Add problem 54 to list of problems

--- prb/054/prb54_spec.js
+++ prb/054/prb54_spec.js
@@ -1,6 +1,6 @@
 describe("Problem #54 function", function() {
   it("finds the correct result", function(done) {
-    readFile('https://raw.githubusercontent.com/christellehilaricus/projecteuler/master/prb/054/poker.txt', function(data){
+    readFile('https://raw.githubusercontent.com/christellehilaricus/projecteuler/master/prb/054/p054_poker.txt', function(data){
       expect(prb54(data)).toEqual(376);
       done();
     });

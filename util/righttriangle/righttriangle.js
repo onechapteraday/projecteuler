8f2724b6404b082dd25f81e0f8351b4a32b30eae
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-11-11 22:22:11 +0100

    Optimize right triangle function and add specific case to test

--- util/righttriangle/righttriangle.js
+++ util/righttriangle/righttriangle.js
@@ -1,15 +1,17 @@
-/* Gives the number of right triangles with perimeter p. */
+/* Gives the number of right triangles with perimeter p.
+ * a^2 + b^2 = c^2
+ * a + b + c = p
+ * so ...
+ * a^2 + b^2 = (p-a-b)^2 = p^2 + a^2 + b^2 - 2pa - 2pb + 2ab
+ * b = (p^2-2pa) / (2p-2a)
+ * so if this equation is an integer, we found a right triangle!
+ * also, a+b>c, values of a should be investiguate until a = p/2+sqrt(2) */
 
 function righttriangle(p){
   var howmany = 0;
-  for(var a = 1; a < p; a++ ){
-    for(var b = a; b < p; b++){
-      if(a+b < 1000){
-        var c = p - a - b;
-        if(a*a + b*b == c*c){
-          howmany++;
-        }
-      } else break;
+  for(var a = 3; a < p/(2+Math.sqrt(2)); a++){
+    if((p*(p-2*a)) % (2*(p-a)) == 0){
+      howmany++;
     }
   }
   return howmany;
--- util/righttriangle/righttriangle_spec.js
+++ util/righttriangle/righttriangle_spec.js
@@ -11,19 +11,7 @@ describe('Right triangles function', function(){
     expect(righttriangle(180)).toBe(3);
   });
 
-//  it('returns false for 375 and 401', function(){
-//    expect(amicable(375,401)).toBe(false);
-//  });
-//
-//  it('returns true for 1184 and 1210', function(){
-//    expect(amicable(1184,1210)).toBe(true);
-//  });
-//
-//  it('returns true for 2620 and 2924', function(){
-//    expect(amicable(2620,2924)).toBe(true);
-//  });
-//
-//  it('returns true for 5020 and 5564', function(){
-//    expect(amicable(5020,5564)).toBe(true);
-//  });
+  it('returns 8 for perimeter p = 840', function(){
+    expect(righttriangle(840)).toBe(8);
+  });
 });

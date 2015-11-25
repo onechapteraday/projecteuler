8abe2fc7a3e15743154da80cb228c1f69729d025
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-11-25 20:38:46 +0100

    Update nth convergent of n function with correct sequence for n

--- prb/066/prb66.js
+++ prb/066/prb66.js
@@ -24,19 +24,23 @@ function issquare(n){
 }
 
 function convergent(n,nth){
-  var sequence = sqrtsequence(n);
+  var tab = sqrtsequence(n),
+      sequence = [];
   var useSeq = function(){
-    var temp = sequence[0];
-    sequence.shift();
-    sequence.push(temp);
+    var temp = tab[0];
+    tab.shift();
+    tab.push(temp);
     return temp;
+  }
+  for(var j = 0; j < nth; j++){
+    sequence.push(useSeq());
   }
   var numerator = new LargeNumber([0]),
       denominator = new LargeNumber([1]);
   for(var i = nth-1; i > 0; i--){
     var num = numerator.getArray(),
         den = denominator.getArray(),
-        seq = new LargeNumber([useSeq()]).getArray();
+        seq = new LargeNumber([sequence[i-1]]).getArray();
     denominator.setArray(addition(num, multiplication(seq,den)));
     numerator.setArray(den);
   }

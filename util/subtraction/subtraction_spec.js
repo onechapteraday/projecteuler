4a843ff3c9edd53c391d9131522fb094f9a3133f
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-11-28 16:27:03 +0100

    Add specs to substraction Jasmine test

--- util/subtraction/subtraction_spec.js
+++ util/subtraction/subtraction_spec.js
@@ -1,27 +1,37 @@
 describe('Subtraction function', function(){
-  var a = subtraction(new LargeNumber('153460854111525838926477').getValue(), new LargeNumber('5874532698962144366844').getValue()),
-      b = subtraction(new LargeNumber('1000000000000000000000').getValue(), new LargeNumber('1').getValue());
-      c = subtraction(new LargeNumber('4378').getValue(), new LargeNumber('4378').getValue()),
-      d = subtraction(new LargeNumber('19998').getValue(), new LargeNumber('9999').getValue()),
-      e = subtraction(new LargeNumber('1333306666670323164113').getValue(), new LargeNumber('99872343237868642879').getValue());
+  var a = subtraction('153460854111525838926477', '5874532698962144366844'),
+      b = subtraction('1000000000000000000000', '1');
+      c = subtraction('4378', '4378'),
+      d = subtraction('19998','9999'),
+      e = subtraction('1333306666670323164113', '99872343237868642879'),
+      f = subtraction('789654', '654987'),
+      g = subtraction('9999', '9998');
 
   it('finds the correct result for a', function(){
-    expect(new LargeNumber(a).getValue()).toEqual('147586321412563694559633');
+    expect(a).toEqual('147586321412563694559633');
   });
 
   it('finds the correct result for b', function(){
-    expect(new LargeNumber(b).getValue()).toEqual('999999999999999999999');
+    expect(b).toEqual('999999999999999999999');
   });
 
   it('finds the correct result for c', function(){
-    expect(new LargeNumber(c).getValue()).toEqual('0');
+    expect(c).toEqual('0');
   });
 
   it('finds the correct result for d', function(){
-    expect(new LargeNumber(d).getValue()).toEqual('9999');
+    expect(d).toEqual('9999');
   });
 
   it('finds the correct result for e', function(){
-    expect(new LargeNumber(e).getValue()).toEqual('1233434323432454521234');
+    expect(e).toEqual('1233434323432454521234');
+  });
+
+  it('finds the correct result for f', function(){
+    expect(f).toEqual('134667');
+  });
+
+  it('finds the correct result for g', function(){
+    expect(g).toEqual('1');
   });
 });

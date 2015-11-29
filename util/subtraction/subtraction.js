14bbe286aa99792692c6e00c5f5eb5a8df29e81d
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2015-11-29 23:24:06 +0100

    Improve the implementation of subtraction with use of 10^7 base

--- util/subtraction/subtraction.js
+++ util/subtraction/subtraction.js
@@ -1,74 +1,29 @@
-/* Propose an implementation of subtraction with String since JavaScript do not
+/* Propose an implementation of subtraction with LargeNumber since JavaScript do not
  * allow big integers. */
 
 function subtraction(x,y){
-  var subtract = ((x,y) => x - y);
-  var add = ((x,y) => x + y);
-
   var xlength = x.length,
       ylength = y.length,
-      length = ylength,
-      result = '',
+      difference = [],
+      result = 0,
       retenue = 0;
 
-  if(ylength>xlength || (xlength==ylength && y>x)){
-    return '';
-  }
+  if (xlength < ylength || (xlength == ylength && xlength[0] < ylength[0]))
+    return undefined;
 
-  for(var i = length-1; i>=0; i--){
-    xlength -= 1;
-    ylength -= 1;
-    var temp = subtract(x[xlength],y[ylength]);
-    if(Math.sign(temp)>-1){
-      result = temp+result;
-    }else{
-      temp = subtract('1'+x[xlength],y[ylength]);
-      if(ylength-1 != -1){
-        if(y[ylength-1]=='9'){
-          if(ylength-2 == -1){
-            y = y.substr(0,ylength-1) + '10' + y.substr(ylength);
-            ylength += 1;
-            i++;
-          }else{
-	    var toadd = ''+add(parseInt(y[ylength-2]),1);
-            y = y.substr(0,ylength-2) + toadd.substr(this.length-1) + '0' + y.substr(ylength);
-	    if(toadd.length>1){
-              if(y[ylength-3]=='9'){
-                if(ylength-4 == -1){
-                  y = y.substr(0,ylength-3) + '10' + y.substr(ylength-2);
-                  ylength += 1;
-                  i++;
-                }
-	      } else {
-	        retenue = 1;
-              }
-	    } 
-          }
-        }else{ 
-          y = y.substr(0,ylength-1) + add(parseInt(y[ylength-1]),1) + y.substr(ylength);
-        }
-      }else{
-        y = '1'+y;
-	ylength += 1;
-	i++;
-      }
-      result = temp+result;
-    }
-  }
-
-  for(var i = xlength-1; i>=0; i--){
-    if(retenue == 1){
-      x = x.substr(0,i) + subtract(parseInt(x[i]),1) + x.substr(i+1);
+  for (var i = xlength; i > 0; i--){
+    xlength--;
+    ylength--;
+    if(xlength>=0 && ylength>=0) result = x[xlength]-(y[ylength]+retenue);
+    else result = x[xlength]-retenue;
+    if (result < 0){
+      retenue = 1;
+      result += 10000000;
+    }else
       retenue = 0;
-      i++;
-    }else{
-      result = x[i]+result;
-    }
+    difference.unshift(result);
   }
 
-  while(result.match(/^0/) && result!='0'){
-    result = result.substr(1);
-  }
-
-  return result;  
+  while(difference[0]==0 && difference.length>1) difference.shift();
+  return difference;
 }
--- util/subtraction/subtraction_spec.js
+++ util/subtraction/subtraction_spec.js
@@ -1,37 +1,42 @@
 describe('Subtraction function', function(){
-  var a = subtraction('153460854111525838926477', '5874532698962144366844'),
-      b = subtraction('1000000000000000000000', '1');
-      c = subtraction('4378', '4378'),
-      d = subtraction('19998','9999'),
-      e = subtraction('1333306666670323164113', '99872343237868642879'),
-      f = subtraction('789654', '654987'),
-      g = subtraction('9999', '9998');
+  var a = subtraction(new LargeNumber('153460854111525838926477').getArray(), new LargeNumber('5874532698962144366844').getArray()),
+      b = subtraction(new LargeNumber('1000000000000000000000').getArray(), new LargeNumber('1').getArray());
+      c = subtraction(new LargeNumber('4378').getArray(), new LargeNumber('4378').getArray()),
+      d = subtraction(new LargeNumber('199998').getArray(), new LargeNumber('99999').getArray()),
+      e = subtraction(new LargeNumber('1333306666670323164113').getArray(), new LargeNumber('99872343237868642879').getArray()),
+      f = subtraction(new LargeNumber('789654').getArray(), new LargeNumber('654987').getArray()),
+      g = subtraction(new LargeNumber('9999').getArray(), new LargeNumber('9998').getArray()),
+      h = subtraction(new LargeNumber('100000000000000').getArray(), new LargeNumber('99999990000001').getArray());
 
   it('finds the correct result for a', function(){
-    expect(a).toEqual('147586321412563694559633');
+    expect(new LargeNumber(a).getValue()).toEqual('147586321412563694559633');
   });
 
   it('finds the correct result for b', function(){
-    expect(b).toEqual('999999999999999999999');
+    expect(new LargeNumber(b).getValue()).toEqual('999999999999999999999');
   });
 
   it('finds the correct result for c', function(){
-    expect(c).toEqual('0');
+    expect(new LargeNumber(c).getValue()).toEqual('0');
   });
 
   it('finds the correct result for d', function(){
-    expect(d).toEqual('9999');
+    expect(new LargeNumber(d).getValue()).toEqual('99999');
   });
 
   it('finds the correct result for e', function(){
-    expect(e).toEqual('1233434323432454521234');
+    expect(new LargeNumber(e).getValue()).toEqual('1233434323432454521234');
   });
 
   it('finds the correct result for f', function(){
-    expect(f).toEqual('134667');
+    expect(new LargeNumber(f).getValue()).toEqual('134667');
   });
 
   it('finds the correct result for g', function(){
-    expect(g).toEqual('1');
+    expect(new LargeNumber(g).getValue()).toEqual('1');
+  });
+
+  it('finds the correct result for h', function(){
+    expect(new LargeNumber(h).getValue()).toEqual('9999999');
   });
 });

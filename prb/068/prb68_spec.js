f3171616f1d61f82c6afe0e10606c4cdebfe2c0b
Christelle Hilaricus <christellehilaricus@fastmail.com>
2015-12-11 20:52:12 +0100

    Add problem 68 solution to Jasmine test
    - For the first time, I found the solution of a problem only with a pencil
      and paper! I will write the algorithm to find this result soon.

--- prb/068/prb68_spec.js
+++ prb/068/prb68_spec.js
@@ -1,5 +1,5 @@
 describe('Problem #68 function', function(){
   it('finds the correct result', function(){
-    expect(prb68()).toEqual(true);
+    expect(prb68()).toEqual('6531031914842725');
   });
 });

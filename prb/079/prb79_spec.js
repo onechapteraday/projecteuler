5f9efab22a82dbb482dada282e750dab9afe6fa7
Christelle Hilaricus <christellehilaricus@fastmail.com>
2016-01-08 13:20:34 +0100

    Add problem 79 context - problem not solved yet!

---
+++ prb/079/prb79_spec.js
@@ -0,0 +1,8 @@
+describe("Problem #79 function", function() {
+  it("finds the correct result", function(done) {
+    readFile('https://raw.githubusercontent.com/christellehilaricus/projecteuler/master/prb/079/p079_keylog.txt', function(data){
+      expect(prb79(data)).toEqual(true);
+      done();
+    });
+  });
+});

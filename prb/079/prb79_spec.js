012782fc6739e42f49ad350332d83a8c6d5deddc
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2016-01-09 13:27:59 +0100

    Add problem 79 solution! Solved with pen and paper
    - Need to do the algorithm for this result!

--- prb/079/prb79_spec.js
+++ prb/079/prb79_spec.js
@@ -1,7 +1,7 @@
 describe("Problem #79 function", function() {
   it("finds the correct result", function(done) {
     readFile('https://raw.githubusercontent.com/christellehilaricus/projecteuler/master/prb/079/p079_keylog.txt', function(data){
-      expect(prb79(data)).toEqual(true);
+      expect(prb79(data)).toEqual(73162890);
       done();
     });
   });

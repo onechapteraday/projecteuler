f90e0e78346d457c4e9de2be17ea114fdfacba25
Christelle Hilaricus <christellehilaricus@fastmail.com>
2016-01-13 13:42:49 +0100

    First draft of a topological sort to solve problem 79
    - Lists are made: with/without predecessors

--- prb/079/prb79.js
+++ prb/079/prb79.js
@@ -6,8 +6,30 @@
  * so as to determine the shortest possible secret passcode of unknown length. */
 
 function prb79(input){
-  // topological sort: http://www.stoimen.com/blog/2012/10/01/computer-algorithms-topological-sort-of-a-graph/
+  // topological sort:
+  // http://www.stoimen.com/blog/2012/10/01/computer-algorithms-topological-sort-of-a-graph/
   var keys = input.split('\n');
   keys.pop();
+  keys.unique();
+  var L = ''; // L: list with no predecessor
+  var P = ''; // P: list with predecessor
+  for(var i=0; i<keys.length; i++){
+    var k1 = keys[i].charAt(1),
+        k2 = keys[i].charAt(2);
+    if(P.indexOf(k1)==-1){
+      P += k1;
+    }
+    if(P.indexOf(k2)==-1){
+      P += k2;
+    }
+  }
+  for(var i=0; i<keys.length; i++){
+    var k0 = keys[i].charAt(0);
+    if(L.indexOf(k0)==-1 && P.indexOf(k0)==-1){
+      L += k0;
+    }
+  }
+  console.log('P: '+P);
+  console.log('L: '+L);
   return true;
 }

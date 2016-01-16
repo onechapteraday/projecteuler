78b588364af3181f7857bfbe4d6f023f81520637
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2016-01-16 21:41:34 +0100

    Rework algorithm to get a number of digits after decimal

--- prb/080/prb80.js
+++ prb/080/prb80.js
@@ -10,9 +10,9 @@ function prb80(){
   // http://www.afjarvis.staff.shef.ac.uk/maths/jarvisspec02.pdf
   var sq = function(n){
     var a = 5*n,
-        b = 5;
-    var i = 50;
-    while(i>0){
+        b = 5,
+	limit = 15;
+    while(b < 10 * Math.pow(10,limit+1)){
       if(a >=b){
         a -= b;
         b += 10;
@@ -20,10 +20,12 @@ function prb80(){
       if(a < b){
         a *= 100;
         b = Math.floor(b/10) * 100 + 5;
+      }
     }
-      i--;
-    }
-    return b;
+    return Math.floor(b/100);
   }
+  // get the first 15 digits after decimal for sqrt(2);
+  // with the integer itself.... so the first digit have to be removed!
+  console.log(parseInt(sq(2).toString().substr(1)));
   return true;
 }

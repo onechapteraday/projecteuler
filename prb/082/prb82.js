6848ca83c3b0a0fa6d6e07cc28c3386fe15dc752
Christelle Hilaricus <christellehilaricus@fastmail.com>
2016-01-21 21:34:03 +0100

    Try to find given example solution

--- prb/082/prb82.js
+++ prb/082/prb82.js
@@ -20,6 +20,13 @@ function prb82(input){
       grid[i][j] = parseInt(grid[i][j]);
     }
   }
+  var given_example =[
+    [131, 673, 234, 103, 18 ],
+    [201, 96 , 342, 965, 150],
+    [630, 803, 746, 422, 111],
+    [537, 699, 497, 121, 956],
+    [805, 732, 524, 37 , 331]
+  ];
   //var minpath = function(){
   //  var cache = new Array();
   //  for(var i = 0 ; i < 80 ; i++){

13a2c6b549fc13f6efa935d8ae5298adce65d064
Christelle Hilaricus <christellehilaricus@fastmail.com>
2016-01-24 22:18:01 +0100

    Work with given example array to find description solution

--- prb/082/prb82.js
+++ prb/082/prb82.js
@@ -27,32 +27,32 @@ function prb82(input){
     [537, 699, 497, 121, 956],
     [805, 732, 524, 37 , 331]
   ];
-  //var minpath = function(){
-  //  var cache = new Array();
-  //  for(var i = 0 ; i < 80 ; i++){
-  //    cache[i] = new Array();
-  //  }
-  //  var min = function(x,y){
-  //    var result = cache[x][y];
-  //    if(typeof(result) == "undefined"){
-  //      if(x==0 && y==0) return grid[x][y];
-  //      else{
-  //        if(y==79){
-  //          return cache[x][y] = grid[x][y] + Math.min(min(x,y-1),min(x-1,y)); /* up or left! */
-  //        }else if(x==0){
-  //          return cache[x][y] = grid[x][y] + Math.min(min(x,y+1),min(x,y-1)); /* down or up */
-  //        }else if(y==0){
-  //          return cache[x][y] = grid[x][y] + Math.min(min(x,y+1),min(x-1,y)); /* down or left */
-  //        }else{ /* point */
-  //          return cache[x][y] = grid[x][y] + Math.min(min(x,y-1),min(x-1,y),min(x,y+1)); /* up or left! or down */
-  //        }
-  //      }
-  //    }else{
-  //      return result;
-  //    }    
-  //  };
-  //  return min;
-  //}();
-  //console.log(minpath(79,79));
+  var minpath = function(){
+    var cache = new Array();
+    for(var i = 0 ; i < 80 ; i++){
+      cache[i] = new Array();
+    }
+    var min = function(x,y){
+      var result = cache[x][y];
+      if(typeof(result) == "undefined"){
+        if(x==0 && y==0) return given_example[x][y];
+        else{
+          if(y==4){
+            return cache[x][y] = given_example[x][y] + Math.min(min(x,y-1),min(x-1,y)); /* up or left! */
+          }else if(x==0){
+            return cache[x][y] = given_example[x][y] + Math.min(min(x,y+1),min(x,y-1)); /* down or up */
+          }else if(y==0){
+            return cache[x][y] = given_example[x][y] + Math.min(min(x,y+1),min(x-1,y)); /* down or left */
+          }else{ /* point */
+            return cache[x][y] = given_example[x][y] + Math.min(min(x,y-1),min(x-1,y),min(x,y+1)); /* up or left! or down */
+          }
+        }
+      }else{
+        return result;
+      }    
+    };
+    return min;
+  }();
+  console.log(minpath(4,4));
   return true;
 }

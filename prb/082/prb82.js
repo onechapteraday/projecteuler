abe8ccacb8aedcbbad939047cf926f2779f88944
Christelle Hilaricus <christellehilaricus@fastmail.com>
2016-01-25 21:20:11 +0100

    Add algorithm to solve problem 82 - works with given example

--- prb/082/prb82.js
+++ prb/082/prb82.js
@@ -13,46 +13,52 @@
  * to the right column. */
 
 function prb82(input){
-  var grid = input.split('\n').map(x => x.split(','));
-  grid.pop();
-  for(var i=0; i<grid.length; i++){
-    for(var j=0; j<grid[i].length; j++){
-      grid[i][j] = parseInt(grid[i][j]);
-    }
-  }
-  var given_example =[
+  //var grid = input.split('\n').map(x => x.split(','));
+  //grid.pop();
+  //for(var i=0; i<grid.length; i++){
+  //  for(var j=0; j<grid[i].length; j++){
+  //    grid[i][j] = parseInt(grid[i][j]);
+  //  }
+  //}
+  var grid =[
     [131, 673, 234, 103, 18 ],
     [201, 96 , 342, 965, 150],
     [630, 803, 746, 422, 111],
     [537, 699, 497, 121, 956],
     [805, 732, 524, 37 , 331]
   ];
-  var minpath = function(){
-    var cache = new Array();
-    for(var i = 0 ; i < 80 ; i++){
-      cache[i] = new Array();
+
+  // find solutions for any cell in the right column
+  var nbrows = grid.length,
+      nbcolumns = grid[0].length,
+      cache = [];
+
+  // add the right column cell as the start point for all rows
+  // this case is a mandatory part of the minimal path
+  for(var i = 0; i < nbrows; i++){
+    cache[i] = grid[i][nbcolumns-1];
+  }
+
+  // then, from the penultimate to the first column
+  // we're first trying to go down, then up, and take the minimum result
+  for(var i = nbcolumns-2; i >= 0; i--){
+    cache[0] += grid[0][i];
+    for(var j = 1; j < nbrows; j++){
+      cache[j] = Math.min(cache[j], cache[j-1]) + grid[j][i];
     }
-    var min = function(x,y){
-      var result = cache[x][y];
-      if(typeof(result) == "undefined"){
-        if(x==0 && y==0) return given_example[x][y];
-        else{
-          if(y==4){
-            return cache[x][y] = given_example[x][y] + Math.min(min(x,y-1),min(x-1,y)); /* up or left! */
-          }else if(x==0){
-            return cache[x][y] = given_example[x][y] + Math.min(min(x,y+1),min(x,y-1)); /* down or up */
-          }else if(y==0){
-            return cache[x][y] = given_example[x][y] + Math.min(min(x,y+1),min(x-1,y)); /* down or left */
-          }else{ /* point */
-            return cache[x][y] = given_example[x][y] + Math.min(min(x,y-1),min(x-1,y),min(x,y+1)); /* up or left! or down */
-          }
-        }
-      }else{
-        return result;
-      }    
-    };
-    return min;
-  }();
-  console.log(minpath(4,4));
+    for(var j = nbrows-2; j >= 0; j--){
+      cache[j] = Math.min(cache[j], cache[j+1] + grid[j][i]);
+    }
+  }
+
+  // find minimal path
+  var min = cache[0];
+  for(var i = 1; i < cache.length; i++){
+    if(min>cache[i]){
+      min = cache[i];
+    }
+  }
+
+  console.log(min);
   return true;
 }

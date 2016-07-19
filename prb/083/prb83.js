34b44c380da519380b46934346a824a42490000c
Christelle Hilaricus <christellehilaricus@fastmail.com>
2016-07-19 23:28:35 +0200

    Add some lines about the pathfinding algorithm in problem 83
    - Work in progress: there are a few missing implementations, like the
      gScore, hScore and fScore which are path scorings.

--- prb/083/prb83.js
+++ prb/083/prb83.js
@@ -31,6 +31,51 @@ function prb83(input){
 
   // Grid example
   var grid = [[131,673,234,103,18],[201,96,342,965,150],[630,803,746,422,111],[537,699,497,121,956],[805,732,524,37,331]];
+  var start = [0,0];
+  var destinationSquare = [grid[0].length-1, grid.length-1];
+
+  var openList = new Set();
+  var closedList = new Set();
+  openList.add(start);
+
+  var neighbours = function([i,j]){
+    var matrix = [[-1,0],[0,-1],[1,0],[0,1]];
+    var n = [];
+    for(var k = 0; k < matrix.length; k++){
+      var x = matrix[k][0];
+      var y = matrix[k][1];
+      if((0 <= i+x) && (0 <= j+y) && (j+y < grid[0].length)){
+        n.push([i+x,j+y]);
+      }
+    }
+    return n;
+  }
+
+  while(openList.size != 0 && !true){
+    // should be the square with the lowest F;
+    var currentSquare;
+    for(let [key, value] of openList.entries()){
+      currentSquare = value;
+      break;
+    }
+
+    closedList.add(currentSquare);
+    openList.delete(currentSquare);
+
+    if(closedList.has(destinationSquare)){
+      break;
+    }
+
+    var adjacentSquares = neighbours(currentSquare);
+    for(var a in adjacentSquares) {
+      if(closedList.has(adjacentSquares[a])){ continue; }
+      if(!openList.has(adjacentSquares[a])){
+        openList.add(adjacentSquares[a]);
+      }else{
+        console.log(adjacentSquares[a]);
+      }
+    }
+  }
 
   return true;
 }

4cb9712d9f5ef79abd2976fa405c98d74d28ca49
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2016-07-21 14:06:31 +0200

    Add thoughts about fScore implementation

--- prb/083/prb83.js
+++ prb/083/prb83.js
@@ -15,11 +15,6 @@
 // introduction to pathfinding: https://www.raywenderlich.com/4946/introduction-to-a-pathfinding
 // things about A* algorithm: https://en.wikipedia.org/wiki/A*_search_algorithm
 
-function A(start, goal) {
-  var closedSet = new Set();
-  var openSet = new Set();
-}
-
 function prb83(input){
   //var grid = input.split('\n').map(x => x.split(','));
   //grid.pop();
@@ -51,10 +46,21 @@ function prb83(input){
     return n;
   }
 
+  var movementCost = function(fromCoord, toCoord){
+    return Math.abs(toCoord[1] - fromCoord[1]) + Math.abs(toCoord[0] - fromCoord[0])
+  }
+
+  var fScore = function(coord){
+    return movementCost(start, coord) + movementCost(coord, destinationSquare);
+  }
+
   while(openList.size != 0 && !true){
     // should be the square with the lowest F;
-    var currentSquare;
+    var currentSquare,
+        min = -1;
+
     for(let [key, value] of openList.entries()){
+      console.log(fScore(value));
       currentSquare = value;
       break;
     }
@@ -67,7 +73,8 @@ function prb83(input){
     }
 
     var adjacentSquares = neighbours(currentSquare);
-    for(var a in adjacentSquares) {
+
+    for(var a = 0; a < adjacentSquares.length; a++) {
       if(closedList.has(adjacentSquares[a])){ continue; }
       if(!openList.has(adjacentSquares[a])){
         openList.add(adjacentSquares[a]);

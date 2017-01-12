05b12589c98d3194147f2a33ee3c61b1f453bdc6
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2017-01-12 15:40:59 +0100

    Add in comment the formula for counting rectangles

--- prb/085/prb85.js
+++ prb/085/prb85.js
@@ -15,5 +15,8 @@
  * rectangles, find the area of the grid with the nearest solution. */
 
 function prb85(){
+  // Assuming we have a rectangular grid n x m
+  // m x (m+1) / 2 x n x (n+1) / 2 is the formula
+  // to find the number of rectangles in the grid
   return true;
 }

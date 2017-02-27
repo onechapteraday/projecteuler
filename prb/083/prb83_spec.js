698530a64300e40b592ff04b99e8805b28b9d753
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2017-02-27 21:45:13 +0100

    Add solved problems to list of all problems

--- prb/083/prb83_spec.js
+++ prb/083/prb83_spec.js
@@ -1,4 +1,4 @@
-describe('Problem #83 given example', function(){
+describe('Problem #83 given example function', function(){
   var grid = [[131, 673, 234, 103,  18],
               [201,  96, 342, 965, 150],
               [630, 803, 746, 422, 111],
@@ -10,7 +10,7 @@ describe('Problem #83 given example', function(){
   });
 });
 
-describe("Problem #83 solution", function() {
+describe("Problem #83 function", function() {
   it("finds the correct result", function(done) {
     readFile('https://raw.githubusercontent.com/christellehilaricus/projecteuler/master/prb/083/p083_matrix.txt', function(data){
       var grid = data.split('\n').map(x => x.split(','));
